f3fc57fdae7e8f5d610ed38b7647dcca33f9aa24
Albert-Jan Nijburg <albertjan@curit.com>
2014-09-24 16:14:19 +0200

    remove stuff

---
+++ src/Castle.ActiveRecord/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Castle.ActiveRecord")]
+[assembly: AssemblyDescription("Active Record Pattern Implementation for .net")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Castle Project")]
+[assembly: AssemblyProduct("Castle.ActiveRecord.Properties")]
+[assembly: AssemblyCopyright("Castle Project Developers")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f40fc0c2-3e84-4c8e-b2a5-70b51a9caffa")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
