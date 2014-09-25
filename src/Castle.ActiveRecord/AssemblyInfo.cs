bf6ffd365e6c72a31b10980749ddfcaac8c84058
Albert-Jan Nijburg <albertjan@curit.com>
2014-09-25 09:15:51 +0200

    Make normal assemblyinfo

--- src/Castle.ActiveRecord/AssemblyInfo.cs
+++ src/Castle.ActiveRecord/AssemblyInfo.cs
@@ -1,6 +1,13 @@
-[assembly: System.Runtime.InteropServices.ComVisible(true)]
-[assembly: System.CLSCompliant(true)]
-[assembly: System.Reflection.AssemblyTitle("Castle.ActiveRecord")]
-[assembly: System.Reflection.AssemblyVersion("3.0.0.0")]
-[assembly: System.Reflection.AssemblyFileVersion("3.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersion("3.0.0.0")]
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Castle Project")]
+[assembly: AssemblyCopyright("Copyright (c) 2004-2010 Castle Project - http://www.castleproject.org")]
+[assembly: ComVisible(true)]
+[assembly: CLSCompliant(true)]
+[assembly: Guid("39838633-CB04-495D-B2EF-4ED02824969A")]
+[assembly: AssemblyTitle("Castle.ActiveRecord")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyInformationalVersion("3.0.0.0")]
